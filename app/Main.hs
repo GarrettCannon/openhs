b3f2d83512b30a7048460c2d26f6797fa129950b
Garrett Cannon <garrett@gcannon.net>
2019-12-27 02:47:31 -0500

    initial

---
+++ app/Main.hs
@@ -0,0 +1,15 @@
+module Main where
+
+import qualified Graphics.UI.GLUT              as GL
+
+main :: IO ()
+main = do
+    GL.getArgsAndInitialize
+    _window <- GL.createWindow "Hello World"
+    GL.displayCallback GL.$= display
+    GL.mainLoop
+
+display :: GL.DisplayCallback
+display = do
+    GL.clear [GL.ColorBuffer]
+    GL.flush
