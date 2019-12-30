ddbf56c6713ff054b440cff3b42919fad48fafb5
Garrett Cannon <garrett@gcannon.net>
2019-12-29 19:42:57 -0500

    added pipe operator

--- app/Main.hs
+++ app/Main.hs
@@ -2,14 +2,19 @@ module Main where
 
 import qualified Graphics.UI.GLUT as GL
 
+(|>) :: a -> (a -> b) -> b
+(|>) x f = f x
+
+infixl 0 |>
+
 main :: IO ()
 main = do
   GL.getArgsAndInitialize
-  _window <- GL.createWindow "Hello World"
+  GL.createWindow "Hello World"
   GL.displayCallback GL.$= display
   GL.mainLoop
 
 display :: GL.DisplayCallback
 display = do
-  GL.clear [GL.ColorBuffer]
+  [GL.ColorBuffer] |> GL.clear
   GL.flush
